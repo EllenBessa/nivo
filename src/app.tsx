4024ff317ce4fbadcbaab602862e72fb777b4241
Ellen Bessa <ellenbessa96@gmail.com>
2024-02-22 19:53:52 -0300

    feat: :sparkles: Finalizando página Home com Radix e React query

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
+import {
+  FileDown,
+  Filter,
+  Loader2,
+  MoreHorizontal,
+  Plus,
+  Search
+} from "lucide-react";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
+import { CreateTagForm } from "./components/create-tag-form";
 import { Header } from "./components/header";
 import { Pagination } from "./components/pagination";
 import { Tabs } from "./components/tabs";
@@ -30,6 +39,7 @@ export interface TagResponse {
 export interface Tag {
   id: string;
   title: string;
+  slug: string;
   amountOfVideos: number;
 }
 
@@ -41,7 +51,11 @@ export function App() {
 
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
-  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+  const {
+    data: tagsResponse,
+    isLoading,
+    isFetching
+  } = useQuery<TagResponse>({
     queryKey: ["get-tags", urlFilter, page],
     queryFn: async () => {
       const response = await fetch(
@@ -76,10 +90,36 @@ export function App() {
       <main className="mx-auto max-w-6xl space-y-5">
         <div className=" flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed bottom-0 right-0 top-0 h-screen min-w-[320px] space-y-10 border-l border-zinc-900 bg-zinc-950 p-10">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold ">
+                    Create tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+                <Dialog.Close />
+
+                <CreateTagForm />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+
+          {isFetching && (
+            <Loader2 className="size-4 animate-spin text-zinc-50" />
+          )}
         </div>
 
         <div className="flex items-center justify-between">
@@ -95,7 +135,7 @@ export function App() {
 
             <Button onClick={handleFilter}>
               <Filter className="size-3" />
-              Filter
+              Apply Filter
             </Button>
           </div>
 
@@ -123,7 +163,7 @@ export function App() {
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
                       <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                      <span className="text-xs text-zinc-500">{tag.slug}</span>
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
